f88e6c8cf2b97e7ecb75f40208e51ec5c26faf61
dwiyuda <dwiyuda@gmail.com>
2024-05-23 16:59:56 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,165 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Penjualan nomor whats app
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+
+        <q-img
+          class="col-5"
+          src="https://cdn.quasar.dev/img/parallax2.jpg"
+        />
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+
+        <q-separator vertical />
+
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://cdn.quasar.dev/img/mountains.jpg"
+        />
+
+        <q-card-actions vertical class="justify-around q-px-md">
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+  </div>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        name="me"
+        :text="['hey, how are you?']"
+        sent
+      />
+      <q-chat-message
+        name="Jane"
+        :text="['doing fine, how r you?']"
+      />
+    </div>
+  </div>
+    </q-drawer>
+
+    <q-page-container>
+    
+  <div class="q-pa-md">
+    <q-carousel
+      v-model="slide"
+      vertical
+      transition-prev="slide-down"
+      transition-next="slide-up"
+      swipeable
+      animated
+      control-color="white"
+      navigation-icon="radio_button_unchecked"
+      navigation
+      padding
+      arrows
+      height="300px"
+      class="bg-purple text-white shadow-1 rounded-borders"
+    >
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <q-icon name="style" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <q-icon name="live_tv" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <q-icon name="layers" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="map" class="column no-wrap flex-center">
+        <q-icon name="terrain" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Penjualan Nomor whatsapp</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      slide: ref('style'),
+      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.',
+      lorem: 'Sedikit image dari penjualan nomor whats app.',
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
